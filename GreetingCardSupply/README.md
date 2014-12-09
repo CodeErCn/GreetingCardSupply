af6cd27116603dcefe7a924b9a1657cae01f28f1
Geoffrey Greenspahn <ggreenspahn@gmail.com>
2014-12-08 17:52:18 -0800

    GG changes, add 6 views, remove extra

--- GreetingCardSupply/README.md
+++
@@ -1,2 +0,0 @@
-This repo is a Demo eCommerce site for Coding Dojo Group Project. Greeting Card Supply.
-Website Build by: Eric Chen, Justin Fisher, Geoffrey Greenspahn.
--- GreetingCardSupply/eCommerce/application/models/admin_model.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  class Admin_model extends CI_model
-  {
-    public function login_validation($post)
-    {
-      //get admin password by email
-    }
-  }
-?>
