d4641bdd4cc0d65944e07ff28650bbf338d5d838
Eric Chen <mouthwood@gmail.com>
2014-12-09 13:53:42 -0800

    finished product index, catergories main, and move pix folder to /assect/file folder

--- eCommerce/application/controllers/products.php
+++ eCommerce/application/controllers/products.php
@@ -5,11 +5,31 @@ class Products extends CI_Controller {
 
   public function index()
   {
-    //get categories
-    //get total of products by category
+    //get categories left join lines
+    $result['categories_lines'] = $this->store_model->get_all_categories();
+    
+    $return['cardEnvelope'] = array();
+    $return['pkgingSolution'] = array();
+    $return['accessories'] = array();
+    $return['shippingRetail'] = array();
+
+    for($i=0, $j=count($result['categories_lines']); $i<$j; $i++) {
+      if($result['categories_lines'][$i]['Ltitle'] == "Cards & Envelopes"){
+        $return['cardEnvelope'][] = $result['categories_lines'][$i]['title'];
+      }
+      if($result['categories_lines'][$i]['Ltitle'] == "Packaging Solutions"){
+        $return['pkgingSolution'][] = $result['categories_lines'][$i]['title'];
+      }
+      if($result['categories_lines'][$i]['Ltitle'] == "Accessories"){
+        $return['accessories'][] = $result['categories_lines'][$i]['title'];
+      }
+      if($result['categories_lines'][$i]['Ltitle'] == "Shipping & Retail"){
+        $return['shippingRetail'][] = $result['categories_lines'][$i]['title'];
+      }
+    }
     //get cart
     //build view
-    $this->load->view('products_index');
+    $this->load->view('products_index', $return);
   }
 
   public function search($post)
@@ -20,7 +40,24 @@ class Products extends CI_Controller {
   }
 
   public function categorymain() {
-    $this->load->view('products_category_main');
+    $result = $this->store_model->get_all_categories();
+    
+    for($i=0, $j=count($result); $i<$j; $i++) {
+      if($result[$i]['Ltitle'] == 'Cards & Envelopes'){
+        $return['lines'][$result[$i]['Ltitle']][] = $result[$i]['title'];
+      }
+      if($result[$i]['Ltitle'] == 'Packaging Solutions'){
+        $return['lines'][$result[$i]['Ltitle']][] = $result[$i]['title'];
+      }
+      if($result[$i]['Ltitle'] == 'Accessories'){
+        $return['lines'][$result[$i]['Ltitle']][] = $result[$i]['title'];
+      }
+      if($result[$i]['Ltitle'] == 'Shipping & Retail'){
+        $return['lines'][$result[$i]['Ltitle']][] = $result[$i]['title'];
+      }
+
+    }
+    $this->load->view('products_category_main', $return);
   }
 
   public function categorydetail($id=null, $start=null)
@@ -32,7 +69,6 @@ class Products extends CI_Controller {
   public function show_item($id=null)
   {
     $this->load->view('products_show_item');
-
     //get the product_by_id
     //get the similar
   }
