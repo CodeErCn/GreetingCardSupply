080da5d6ed03117921a5ed68bcce28bb96c94c6c
Justin L. Fisher <jfisher@wrival.com>
2014-12-09 07:15:31 -0800

    DB Inline Products table changes.

--- eCommerce/application/views/category_main.php
+++ eCommerce/application/views/category_main.php
@@ -1,89 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Categories Main</title>
-    <link rel="stylesheet" type="text/css" href="/assets/css/category_main.css">
-  </head>
-  <body>
-    <div class="categories">
-      <div class="cards-n-envelop">
-        <h2>Cards and Envelops</h2>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
+
+<!-- Home Page... -->
+    
+    <?php 
+    foreach($lines as $line) {
+    ?>
+
+      <div class="categories">
+        
+        <h2><?= $lines['title']?></h2>
+        <?=php 
+          foreach($categories as $category) {
+        ?>
+        
+        <div>
+          <img src="/pix/category/<?= $category['id'] ?>_thumb.png" alt="<?= $category['title'] ?>">
+          <h6><a href="/category/<?= $category['id'] ?>"><?= $category['title'] ?></a></h6>
+        </div>
+        
+        <?=php 
+          }
+        ?>
+        
       </div>
-    <!-- END of cards & envelops -->
-      <div class="pk-solution">
-        <h2>Packaging and Solution</h2>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-      </div>
-    <!-- END of packaging solution -->
-      <div class="accessories">
-        <h2>Accessories</h2>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-           <div>
-            <img src="#" alt=""/>
-          </div>
-      </div>
-    <!-- END of accessories -->
-      <div class="shipping-n-retail">
-        <h2>Shipping and Retail</h2>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-          <div>
-            <img src="#" alt=""/>
-          </div>
-      </div>
-    <!-- END of shipping and retail -->
-    </div>
-  <!-- END of categories -->
-  </body>
-</html>
+      
+    <?php
+      }
+    ?>
+
+<!-- ...Home Page -->
