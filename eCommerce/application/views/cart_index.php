7eeeb10f0da131e904bf2983560b20656872d81b
Justin L. Fisher <jfisher@wrival.com>
2014-12-10 19:06:56 -0800

    Autoload models, cart updates

--- eCommerce/application/views/cart_index.php
+++ eCommerce/application/views/cart_index.php
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 	
 	
 	<style scoped>
 		
-
 		#cart-table {
 			width: 100%;
 		}
@@ -24,6 +22,7 @@
 			font-weight: bold;
 			font-size: 200%;
 		}
+
 
 		#checkoutForm div label {
 			display: inline-block;
@@ -56,8 +55,6 @@
 		#bill_yr {
 			width: 60px;
 		}
-
-
 	</style>
 	
 
@@ -77,7 +74,9 @@
 		$subtotal = 0;
 		foreach($items AS $item) {
 			$amount = $item['qty'] * $item['price'];
+			$amount = number_format($amount, 2);
 			$subtotal += $amount;
+			$subtotal = number_format($subtotal, 2);
 			?>
 			<tr>
 				<td><?= $item['id'] ?></td>
@@ -89,6 +88,7 @@
 			<?php
 		}
 		$total = $subtotal + 1;
+		$total = number_format($total, 2);
 		?>
 		<tr>
 			<th class="right" colspan="4">Subtotal: </th>
@@ -106,8 +106,7 @@
 
 
 	<div>
-		<button>Checkout</button>
-		<button>Continue Shopping</button>
+		<a href="/">Continue Shopping</a>
 	</div>
 
 
@@ -206,68 +205,3 @@
 
 
 	</form>
-=======
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Cart</title>
-      <link rel="stylesheet" type="text/css" href="/assets/css/cart_index.css">
-    </head>
-    <body>
-      <div class="top">
-        <table class="table" border = 1>
-          <thead style="background-color: light gray">
-            <tr>
-              <th>Order ID</th>
-              <th>Name</th>
-              <th>Date</th>
-              <th>Billing Address</th>
-              <th>Total</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-        </table>
-        <div class="buttons">
-          <button>Checkout</button>
-          <button>Continue Shopping</button>
-        </div>
-      </div>
-    <!-- END of top -->
-      <div class="shipping">
-        <h2>Shipping Information</h2>
-        <form>
-          <label>First Name: <input type="text"/></label>
-          <label>Last Name: <input type="text"/></label>
-          <label>Email: <input type="text"/></label>
-          <label>Phone Number:  <input type="text"/></label>
-          <label>Address: <input type="text"/></label>
-          <label>Address2:  <input type="text"/></label>
-          <label>City:  <input type="text"/></label>
-          <label>State: <input type="text"/></label>
-          <label>Zip code:  <input type="text"/></label>
-        </form>
-      </div>
-    <!-- END of shipping -->
-      <div class="billing">
-        <h2>Billing Information</h2>
-        <form>
-          <label><input type="checkbox"/> Same as Shipping</label>
-          <label>First Name: <input type="text"/></label>
-          <label>Last Name: <input type="text"/></label>
-          <label>Email: <input type="text"/></label>
-          <label>Phone Number:  <input type="text"/></label>
-          <label>Address: <input type="text"/></label>
-          <label>Address2:  <input type="text"/></label>
-          <label>City:  <input type="text"/></label>
-          <label>State: <input type="text"/></label>
-          <label>Zip code:  <input type="text"/></label>
-          <label>Card:  <input type="text"/></label>
-          <label>Billing DAte: <input type="text"/></label>
-          <label>Expiration: <input class="month" type="text"/> / <input class="date" type="text"></label>
-        </form>
-      </div>
-    <!-- END of billing -->
-    </body>
-</html>
->>>>>>> bf3757a9b5dfb2d81dfeeeb2895da4535b351f0b
